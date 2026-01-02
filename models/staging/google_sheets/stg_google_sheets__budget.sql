894f206dd091b31293da05324d22af847b5b4655
Sathyanarayana6 <83915752+Sathyanarayana6@users.noreply.github.com>
2026-01-01 19:57:12 -0600

    Create stg_google_sheets__budget.sql

---
+++ models/staging/google_sheets/stg_google_sheets__budget.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized='incremental',
+    unique_key = 'budget_key'
+    ) 
+}}
+
+WITH src_budget AS (
+    SELECT *
+    FROM {{ source('google_sheets', 'budget') }}
+{% if is_incremental() %}
+
+	  where _fivetran_synced > (select max(date_loaded) from {{ this }})
+
+{% endif %}
+    ),
+
+stg_budget AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['product_id', 'month']) }} AS budget_key,
+        {{ dbt_utils.generate_surrogate_key(['product_id']) }} AS product_key,
+        {{ dbt_utils.generate_surrogate_key(['month']) }} AS date_key,
+        quantity::INT as quantity,
+        _fivetran_synced as date_loaded 
+    FROM src_budget
+    )
+
+SELECT * FROM stg_budget
